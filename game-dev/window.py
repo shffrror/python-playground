aa73504bee278496107d7b310de882ebb29f2426
shffrror <shffrror@pm.me>
2021-10-09 21:50:42 -0400

    Rendering pretty red box

--- game-dev/window.py
+++ game-dev/window.py
@@ -7,7 +7,10 @@ done = False
 
 while not done:
     for event in pygame.event.get():
+        rectangle = pygame.Rect(10, 30, 13, 13)
+        RED = (255, 30, 70)
+        pygame.draw.rect(screen, RED, rectangle)
         if event.type == pygame.QUIT:
             done = True
-    pygame.display().flip()
+    pygame.display.flip()
 
