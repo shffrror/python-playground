b28e9de06b1ce6517616de8adcde5108670090b4
shffrror <shffrror@pm.me>
2021-10-10 21:01:36 -0400

    Small cleanup

--- game-dev/window.py
+++ game-dev/window.py
@@ -1,7 +1,12 @@
 import pygame
 
+
+BOARD_SIZE_X = 400
+BOARD_SIZE_Y = 400
+RED = (255, 30, 70)
+
 pygame.init()
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode((BOARD_SIZE_X, BOARD_SIZE_Y))
 
 done = False
 rectangle_x =  10
@@ -10,10 +15,8 @@ rectangle_y = 30
 while not done:
     for event in pygame.event.get():
         rectangle = pygame.Rect(rectangle_x, rectangle_y, 13, 13)
-        RED = (255, 30, 70)
         pygame.draw.rect(screen, RED, rectangle)
         pressed = pygame.key.get_pressed()
-
         if(pressed[pygame.K_LEFT]):
             if(rectangle_x - 5 > 0):
                 rectangle_x = rectangle_x - 5
